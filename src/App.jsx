13c08f605642ef4cdf2eb3f7767c5c1990870b95
eduardoreyes007351208 <lalosoccer307@gmail.com>
2026-01-04 18:27:26 -0800

    feat: Create function that update state of user URL input

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [recipeURL, setRecipeURL] = useState('')
+  let localURL = `http://127.0.0.1:8000/get_pdf?url=${recipeURL}`
+
+  const getPDF = (event) => {
+    event.preventDefault()
+    console.log(recipeURL)
+    console.log(localURL)
+    setRecipeURL('')
+  }
 
   return (
     <>
       <div>
         <h1>Recipe PDF Generator</h1>
-        <form action="submit">
-          <input type="text" />
-          <button></button>
+        <form onSubmit={getPDF}>
+          <input value={recipeURL} type="text" onChange={(e) => setRecipeURL(e.target.value)} placeholder='Enter URL'/>
+          <button type='submit'>Search</button>
         </form>
       </div>
     </>
