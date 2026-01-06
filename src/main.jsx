c5f62b7bf7c343850c11b2ec21a4cbe9b573233d
eduardoreyes007351208 <lalosoccer307@gmail.com>
2026-01-06 06:27:58 -0800

    fix: Setting size of all components

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import Instructions from './components/Instructions.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Instructions from "./components/Instructions.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div id='mainDiv'>
-      <Header />
-      <App />
-      <Instructions />
-      <Footer />
-    </div>
-  </StrictMode>,
-)
+    <Header />
+    <App />
+    <Instructions />
+    <Footer />
+  </StrictMode>
+);
