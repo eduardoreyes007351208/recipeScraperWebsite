20b68cb2e8011d461dc2cc2e4a2da2a9247917bf
eduardoreyes007351208 <lalosoccer307@gmail.com>
2026-01-06 18:11:45 -0800

    feat: Style the header div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
+    <div id='headerDiv'>
         <h1>Recipe Scraper PDF Generator</h1>
     </div>
   )
