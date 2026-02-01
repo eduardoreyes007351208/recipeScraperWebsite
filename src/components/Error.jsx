8914c00cf83d2c685a301e5c10b3a0afe032d278
eduardoreyes007351208 <lalosoccer307@gmail.com>
2026-02-01 13:01:05 -0800

    feat: Style the error container

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,10 @@ import './Error.css'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div id='errorMessageContainer'>
+        <h2 className='errorHeading2'>ERROR! Was not able to received data from URL</h2>
+        <h4 className='errorHeading4'>Please try again with a new URL</h4>
+    </div>
   )
 }
 
